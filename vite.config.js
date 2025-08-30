dc1aee49649715bc1ab01bbb2983a0011e381bd2
charith <tilershub@gmail.com>
2025-08-30 07:41:53 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  // Handle routing for SPA
+  base: '/',
+  publicDir: 'public'
+})
