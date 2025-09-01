a8fa57b73dffcb11d9a69141b6bf079b424a1a5c
charith <tilershub@gmail.com>
2025-09-01 19:32:55 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './components/Header.module.css';
+import '../Header.module.css';
 
 export default function Header() {
   const location = useLocation();
